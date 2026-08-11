ae9fa77430223d2e141a4486a2130014a8cb97a2
aidanhendrix00-cmd <aidanhendrix00@gmail.com>
2026-08-10 19:33:02 -0700

    Update Enterprise pricing: $2,000-$5,500+/mo, implementation $4,000-$10,000+

--- src/worker.js
+++ src/worker.js
@@ -428,7 +428,9 @@ const PRICING_LINKS_SCRIPT = '<script>' +
   'var tiersRow=document.getElementById("tiersRow");' +
   'if(!tiersRow){return;}' +
   'var entPrice=tiersRow.querySelector(".tier.enterprise .tier-price");' +
-  'if(entPrice){entPrice.innerHTML="$2,000<span class=\\"per\\">–$5,500/mo</span>";}' +
+  'if(entPrice){entPrice.innerHTML="$2,000<span class=\\"per\\">–$5,500+/mo</span>";}' +
+  'var entImpl=tiersRow.querySelector(".tier.enterprise .tier-impl b");' +
+  'if(entImpl){entImpl.textContent="$4,000–$10,000+";}' +
   'var tierBtns=tiersRow.querySelectorAll("a.btn-tier");' +
   'for(var i=0;i<tierBtns.length;i++){' +
   '(function(a){' +
